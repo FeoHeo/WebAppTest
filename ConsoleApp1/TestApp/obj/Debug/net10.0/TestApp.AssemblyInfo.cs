5e6ea107191e06ddca00d54c99caa85d5c0e1052
Seth <nguyenduong8bc1@gmail.com>
2026-01-21 14:52:14 +0700

    Fixed db not connecting

--- ConsoleApp1/TestApp/obj/Debug/net10.0/TestApp.AssemblyInfo.cs
+++ ConsoleApp1/TestApp/obj/Debug/net10.0/TestApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93857e26cb91b8d9cf7c0efb3ce1fa1f1fb8c59a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a7421343bdc9e3858c1f802b924e602bf5d3425")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
